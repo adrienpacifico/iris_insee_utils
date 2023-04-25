a51d6b9496cbabc7a231228042fbc02f5501c3c4
adrienpacifico <adrienpacifico@pop-os.localdomain>
2023-04-25 22:37:15 +0200

    ajoute code coverage a pytest, change les tests pour utiliser les paths par default

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ install_library:
 download_and_initialize_data:
 	python ./iris_insee_utils/download_data.py
 launch_test:
-	pytest -n auto iris_insee_utils
+	pytest -n auto --cov=iris_insee_utils --cov-report=xml
--- iris_insee_utils/tests/test_download_data.py
+++ iris_insee_utils/tests/test_download_data.py
@@ -1,24 +1,19 @@
 import iris_insee_utils
-from iris_insee_utils.download_data import download, unzip_downloaded_file
-from iris_insee_utils.prepare_data import prepare_data_
+from iris_insee_utils.download_data import download, unzip_downloaded_file, prepare_data_
 
 
 
 
 url = "https://wxs.ign.fr/1yhlj2ehpqf3q6dt6a2y7b64/telechargement/inspire/CONTOURS-IRIS-2018-01-02$CONTOURS-IRIS_2-1__SHP__FRA_2018-01-01/file/CONTOURS-IRIS_2-1__SHP__FRA_2018-01-01.7z"
 
-data_path = iris_insee_utils.__path__[0]+'/../data'
+DATA_PATH = iris_insee_utils.__path__[0]+'/../data'
+file_to_write_path = DATA_PATH+"/raw/iris_2018.7z"
 
-#def test_downlad():
-#    download(url, "data/to_trash666.25avril")
+def test_downlad():
+    download(url, DATA_PATH+"/raw/iris_2018.7z")
 
-#def test_unzip_downloaded_file():
-#    unzip_downloaded_file(data_path+"/to_trash666.25avril")
+def test_unzip_downloaded_file():
+    unzip_downloaded_file(file_to_write_path, DATA_PATH+"/raw")
 
 def test_prepare_data():
-    prepare_data_()
-
-
-
-if __name__ == "__main__":
-    test_gps_to_iris()
+    prepare_data_(DATA_PATH)
