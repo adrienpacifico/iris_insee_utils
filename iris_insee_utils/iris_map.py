fe29d2ffe922b3178a73d557714d99a7043cce39
adrien pacifico <adrienpacificopro@gmail.com>
2024-10-30 11:30:11 +0100

    Add df_oi to the iris plotting function

--- iris_insee_utils/iris_map.py
+++ iris_insee_utils/iris_map.py
@@ -1,5 +1,7 @@
 """This module is test that gps coordinates are linking to the correct IRIS."""
+import os
 
+import iris_insee_utils
 from iris_insee_utils.gps_to_iris import gps_to_iris
 import pandas as pd
 from loguru import logger
@@ -7,43 +9,58 @@ import geopandas as gpd
 import folium
 from tqdm import tqdm
 
-def plot_folium_map(iris_year=None, commune_name="Marseille", df_oi: pd.DataFrame=None, df_enrich: pd.DataFrame=None,
+
+from loguru import logger
+
+def plot_folium_map(iris_year, commune_name="Marseille", df_oi: pd.DataFrame=None, df_enrich: pd.DataFrame=None,
                     df_enrich_iriscol_colname=None, df_enrich_select_cols=None, save_map=False):
     """
     Plot a folium map of iris, for a given commune, or departement number.
     """
-    df_map = gpd.read_parquet(f"data/transformed/iris_{iris_year}.parquet").to_crs(epsg=4326) # TODO: add this to the cleaning function
+
+    file_path = os.path.join(os.path.dirname(iris_insee_utils.__file__),
+                                f"../data/transformed/iris_{iris_year}.parquet")
+                                
+
+    df_map = gpd.read_parquet(file_path).to_crs(epsg=4326) # TODO: add this to the cleaning function
     df_map["NOM_COM"]=df_map.NOM_COM.str.strip() # remove leading and trailing spaces, TODO: add this to the cleaning function
     df_map = df_map[df_map.NOM_COM.str.contains(commune_name,case=True)]#df_map = df_map.query("NOM_COM == @commune_name")
-    print(df_map)
-    map = folium.Map(location=[df_map.geometry.centroid.y.mean(), df_map.geometry.centroid.x.mean()], zoom_start=12)
-    for _, r in df_map.iterrows():
-        # Without simplifying the representation of each borough,
-        # the map might not be displayed
-        sim_geo = gpd.GeoSeries(r['geometry'])#.simplify(tolerance=0.001)
+    m = folium.Map(location=[df_map.geometry.centroid.y.mean(), df_map.geometry.centroid.x.mean()], zoom_start=12)
+    for _, row in df_map.iterrows():
+        sim_geo = gpd.GeoSeries(row['geometry'])#.simplify(tolerance=0.001)
         geo_j = sim_geo.to_json()
         geo_j = folium.GeoJson(data=geo_j,smooth_factor=.1,
                             style_function=lambda x: {'fillColor': 'lightblue'})
-        folium.Popup("Code iris: "+r['CODE_IRIS']+"<br>"
-                    +"nom iris: "+ r['NOM_IRIS'] + "<br>"
-                    + "Commune: "+ r['NOM_COM'] + "<br>"
+        folium.Popup("Code iris: "+row['CODE_IRIS']+"<br>"
+                    +"nom iris: "+ row['NOM_IRIS'] + "<br>"
+                    + "Commune: "+ row['NOM_COM'] + "<br>"
                 
                     ).add_to(geo_j)
-        geo_j.add_to(map)
-    
-    
+        geo_j.add_to(m)
 
+    if df_oi is not None:
 
+        for _, row in df_oi.iterrows():
+            location = [row["lat"], row["lon"]]
+            tooltip = ""
+            for column_name, value in row.items():
+                if column_name not in ["lat", "lon"]:
+                    tooltip += f"{column_name}: {value}<br>"
+            
+            m.add_child(folium.Marker(location=[row["lat"], row["lon"]],#icon=icon, 
+                                tooltip=  tooltip
+                                )
+                    )
 
     if df_enrich:
         raise NotImplementedError("This functionality has not been implemented yet.")
     if save_map:
         raise NotImplementedError("This functionality has not been implemented yet.")
         # 
-        map.save("index.html")
+        m.save("index.html")
     
     logger.warning("Test")
-    return map
+    return m
 
 
 
--- tests/test_folium_map.py
+++ tests/test_folium_map.py
@@ -1,23 +1,34 @@
 import folium
 import unittest
 from iris_insee_utils.iris_map import plot_folium_map
+import pandas as pd
 
 class TestFoliumMap(unittest.TestCase):
 
     def test_plot_folium_map(self):
         # Call the function to generate the map
-        map = plot_folium_map(commune_name="Nice", iris_year=2018)
+        m = plot_folium_map(commune_name="Nice", iris_year=2018)
 
         # Check if the returned object is a Folium Map
-        self.assertIsInstance(map, folium.Map)
+        self.assertIsInstance(m, folium.Map)
 
         # Generate HTML content
-        html_content = map.get_root().render()
+        html_content = m.get_root().render()
 
         print(html_content[:2000])
         # Check if the HTML content contains expected elements
         self.assertIn('<div class="folium-map"', html_content)
         self.assertIn('Nice', html_content)  # Check if the commune name is in the HTML
+
+    def test_plot_folium_map_with_df_oi(self):
+        df_oi = pd.DataFrame({
+            'Lieu': ['Mairie de Marseille', 'Site-Mémorial du Camp des Milles'],
+            'lon': [5.369905252590892, 5.382786610618382],
+            'lat': [43.296630332564405, 43.5034655315141]
+        })
+        
+        m = plot_folium_map(iris_year=2018, commune_name="Marseille", df_oi=df_oi)
+        self.assertIsInstance(m, folium.Map)
 
 if __name__ == '__main__':
     unittest.main()
