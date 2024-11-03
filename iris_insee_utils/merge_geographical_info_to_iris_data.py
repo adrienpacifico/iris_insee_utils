8b097891a42319743e8675ec6a3a03aba1d6cdaf
adrienpacifico <adrienpacifico@gmail.com>
2024-11-03 16:35:14 +0100

    Change imports for end_user, add a helper to get only iris code for gps_to_iris, rename files to avoid confusion between function and file (#10)
    * try to skip main guard tests with   # pragma: no cover
    
    * prevent codecov from breaking
    
    * -Add imports more compatible with user api
    - change file name for gps_coordinates to prevent confusion between function and file name
    
    ---------
    
    Co-authored-by: adrien pacifico <adrienpacificopro@gmail.com>

--- iris_insee_utils/merge_geographical_info_to_iris_data.py
+++ iris_insee_utils/merge_geographical_info_to_iris_data.py
@@ -6,7 +6,7 @@ by using IGN geographical dat to join the two datasets.
 import pandas as pd
 import geopandas as gpd
 
-from iris_insee_utils.gps_to_iris import df_gps_to_iris
+from iris_insee_utils.gps_coordinates_to_iris import df_gps_to_iris
 
 
 def merge_gps_dataset_to_iris_dataset(
